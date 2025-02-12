745ceba930ba2bb753ef0d3e2398eb62c254fdce
Vinaydeep Singh Padda <vinaydeepsinghpadda@Vinaydeeps-MacBook-Pro.local>
2025-02-12 01:42:55 -0500

    Warehouse and parts created

--- Sources/Inventory/Data/ExampleDataModel.swift
+++ Sources/Inventory/Data/ExampleDataModel.swift
@@ -9,10 +9,30 @@ import Foundation
 import Hummingbird
 
 // This is an example data model object.
-struct ExampleDataModel {
+struct Part {
     let id: UUID
     let name: String
+    let category: Category
+    let size: Size
+    let weight: Double
 }
 
+struct Warehiuse {
+    let id: UUID
+    let name: String
+    let location:  [Location]
+    let contactNumber: String
+    let manager: String
+}
+
+
+struct Size {
+    
+}
+
+
 // Every data model you create MUST contain this line (DON'T forget to change the object name)
-extension ExampleDataModel: ResponseEncodable, Decodable, Equatable {}
+extension Part: ResponseEncodable, Decodable, Equatable {}
+extension WareHouse: ResponseEncodable, Decodable, Equatable {}
+extension Size: ResponseEncodable, Decodable, Equatable {}
+extension Location: ResponseEncodable, Decodable, Equatable {}
