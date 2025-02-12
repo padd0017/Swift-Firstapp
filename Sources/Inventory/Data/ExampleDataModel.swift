2169c5a7a87ac514c36f1d35aaa6fd284f823de6
Vinaydeep Singh Padda <vinaydeepsinghpadda@Vinaydeeps-MacBook-Pro.local>
2025-02-12 02:11:47 -0500

    parts created

--- Sources/Inventory/Data/ExampleDataModel.swift
+++ Sources/Inventory/Data/ExampleDataModel.swift
@@ -9,6 +9,16 @@ import Foundation
 import Hummingbird
 
 // This is an example data model object.
+
+enum Category: String, Codable {
+    case engine
+    case transmission
+    case breaks
+    case body
+    
+}
+
+
 struct Part {
     let id: UUID
     let name: String
@@ -17,7 +27,7 @@ struct Part {
     let weight: Double
 }
 
-struct Warehiuse {
+struct WareHouse {
     let id: UUID
     let name: String
     let location:  [String]
@@ -31,5 +41,3 @@ struct Warehiuse {
 // Every data model you create MUST contain this line (DON'T forget to change the object name)
 extension Part: ResponseEncodable, Decodable, Equatable {}
 extension WareHouse: ResponseEncodable, Decodable, Equatable {}
-extension Size: ResponseEncodable, Decodable, Equatable {}
-extension Location: ResponseEncodable, Decodable, Equatable {}
